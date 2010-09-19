8a8f2f487632540bdeccdb6cb04329f1702f75b3
Keegan McAllister <mcallister.keegan@gmail.com>
2010-09-19 01:39:50 -0400

    progress dots on train

--- Detrospector/Modes/Train.hs
+++ Detrospector/Modes/Train.hs
@@ -6,19 +6,33 @@ module Detrospector.Modes.Train(train) where
 import Detrospector.Types
 import Detrospector.Modes
 
-import qualified Data.Text.Lazy    as Txt
-import qualified Data.Text.Lazy.IO as Txt
+import System.IO
+import Control.Monad
+import qualified Data.Text         as TS
+import qualified Data.Text.Lazy    as TL
+import qualified Data.Text.Lazy.IO as TL
 import qualified Data.HashMap      as H
 import qualified Data.IntMap       as IM
 import qualified Data.Sequence     as S
 import qualified Data.Foldable     as F
 
+-- foldl' with progress dots
+progFold :: Int -> (a -> b -> a) -> a -> [b] -> IO a
+progFold rate f = go 0 where
+  go _  !v [] = return v
+  go !n !v (x:xs) = do
+    when ((n `mod` rate) == 0) $ putChar '.'
+    go (n+1) (f v x) xs
+
 -- Build a Markov chain with n-Char history from some input text.
 train :: ModeFun
 train Train{num,out} = do
-  ys <- Txt.getContents
-  let hm = H.map cumulate . snd $ Txt.foldl' roll (emptyQ,H.empty) ys
-  writeChain out $ Chain num hm where
+  hSetBuffering stdout NoBuffering
+  ys <- TL.getContents
+  putStr "Calculating"
+  (_,h) <- progFold 1 (TS.foldl' roll) (emptyQ,H.empty) $ TL.toChunks ys
+  putStrLn "done."
+  writeChain out . Chain num $ H.map cumulate h where
 
   roll (!s,!h) x
     = (shift num x s, F.foldr (H.alter $ ins x) h $ S.tails s)
