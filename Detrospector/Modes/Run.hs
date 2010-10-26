b4f23f62d197f05683da7c033e1c0c04f3d5299a
Keegan McAllister <mcallister.keegan@gmail.com>
2010-10-25 21:36:45 -0400

    tweaks

--- Detrospector/Modes/Run.hs
+++ Detrospector/Modes/Run.hs
@@ -7,10 +7,10 @@ import Detrospector.Modes
 
 go :: Chain -> RNG -> IO ()
 go c@(Chain n _) g = loop emptyQ where
-    loop s = do
-      (x,_) <- pick c s g
-      putChar x
-      loop $! shift n x s
+  loop s = do
+    (x,_) <- pick c s g
+    putChar x
+    loop $! shift n x s
 
 run :: ModeFun
 run Run{chain} = withChain chain go
